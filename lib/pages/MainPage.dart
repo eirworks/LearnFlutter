47b734532ab0bbc577ae5d1b0133dc78bb387033
Rully <hvsciverius@gmail.com>
2022-10-28 19:06:29 +0700

    using app bar for title

--- lib/pages/MainPage.dart
+++ lib/pages/MainPage.dart
@@ -11,7 +11,6 @@ import '../components/MenuListItem.dart';
 import 'learn/Dicee.dart';
 
 class MainPage extends StatelessWidget {
-
   MainPage({Key? key}) : super(key: key);
 
   final words = Get.put(EnglishWOrdsController());
@@ -20,92 +19,94 @@ class MainPage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.grey.shade200,
-      body: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        // crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Container(
-            margin: EdgeInsets.symmetric(vertical: 10.0),
-            child: Container(
-              margin: EdgeInsets.symmetric(horizontal: 10.0),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Icon(
-                    Icons.book,
-                    size: 40.0,
-                  ),
-                  const SizedBox( width: 10.0,),
-                  Expanded(
-                    child: Column(
-                      children: [
-                        const Text(
-                          "Flutter Daily",
-                          style: TextStyle(fontFamily: "Playfair", fontSize: 25.0),
-                        ),
-                        Text(
-                          "Learn Flutter once a day",
-                          style: TextStyle(fontFamily: "Playfair", fontStyle: FontStyle.italic, color: Colors.grey.shade700),
-                        ),
-                      ],
-                    ),
-                  ),
-                  TextButton(onPressed: () {}, child: Text("Subscribe", style: const TextStyle(color: Colors.teal),))
-                ],
+      appBar: AppBar(
+          centerTitle: true,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: Icon(
+                Icons.person_rounded,
+                color: Colors.grey[800],
+                size: 36,
               ),
+            )
+          ],
+          backgroundColor: Colors.transparent,
+          shadowColor: Colors.transparent,
+          leading: const FlutterLogo(
+            size: 16,
+          ),
+          title: Column(
+            children: [
+              Text(
+                "Flutter Daily",
+                style: TextStyle(
+                    fontFamily: "Playfair",
+                    fontSize: 25.0,
+                    color: Colors.grey[800],
+                    fontWeight: FontWeight.w600),
+              ),
+              Text(
+                "Learn Flutter once a day",
+                style: TextStyle(
+                    fontFamily: "Playfair",
+                    fontStyle: FontStyle.italic,
+                    color: Colors.grey.shade700),
+              ),
+            ],
+          )),
+      body: SingleChildScrollView(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          // crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            MenuListItem(
+              title: "Hello World",
+              description: "Basic Hello World text in the center of page",
+              onTap: () => Get.to(const HelloWorld()),
             ),
-          ),
-          MenuListItem(
-            title: "Hello World",
-            description: "Basic Hello World text in the center of page",
-            onTap: () => Get.to(HelloWorld()),
-          ),
-          MenuListItem(
-            title: "Business Card",
-            description:
-                "Simple business card with Copy/Clipboard, Column, Row, Snackbar",
-            onTap: () => Get.to(BusinessCard()),
-          ),
-          MenuListItem(
-            title: "Dicee",
-            description:
-                "Dice rolling app to display 2 6-faced dies.",
-            onTap: () => Get.to(Dicee()),
-          ),
-          MenuListItem(
-            title: "Not My Word",
-            description:
-                "List of dynamic items",
-            onTap: () {
-              words.resetWords();
-              Get.to(EnglishPlease());
-            },
-          ),
-          MenuListItem(
-            title: "UI Catalog",
-            description:
-                "List of UIs",
-            onTap: () {
-              Get.to(UiCatalog());
-            },
-          ),
-          MenuListItem(
-            title: "Settings and Preferences",
-            description:
-                "Shared preferences and settings look",
-            onTap: () {
-              print("Feature not available yet!");
-            },
-          ),
-          MenuListItem(
-            title: "API Call",
-            description:
-                "Learn on how to call API",
-            onTap: () {
-              Get.to(LearnApi());
-            },
-          ),
-        ],
+            MenuListItem(
+              title: "Business Card",
+              description:
+                  "Simple business card with Copy/Clipboard, Column, Row, Snackbar",
+              onTap: () => Get.to(const BusinessCard()),
+            ),
+            MenuListItem(
+              title: "Dicee",
+              description: "Dice rolling app to display 2 6-faced dies.",
+              onTap: () => Get.to(Dicee()),
+            ),
+            MenuListItem(
+              title: "Not My Word",
+              description: "List of dynamic items",
+              onTap: () {
+                words.resetWords();
+                Get.to(EnglishPlease());
+              },
+            ),
+            MenuListItem(
+              title: "UI Catalog",
+              description: "List of UIs",
+              onTap: () {
+                Get.to(UiCatalog());
+              },
+            ),
+            MenuListItem(
+              title: "Settings and Preferences",
+              description: "Shared preferences and settings look",
+              onTap: () {
+                debugPrint("Feature not available yet!");
+              },
+            ),
+            MenuListItem(
+              title: "API Call",
+              description: "Learn on how to call API",
+              onTap: () {
+                Get.to(LearnApi());
+              },
+            ),
+          ],
+        ),
       ),
     );
   }
